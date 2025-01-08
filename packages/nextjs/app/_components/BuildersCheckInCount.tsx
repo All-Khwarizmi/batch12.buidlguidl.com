4c22d8628fe30ffd7af6a3391f3b259c2619c6dc
Jason Suarez <suarezjason@icloud.com>
2025-01-08 06:23:46 +0100

    Scaffolding: create builders checkin compoScaffolding: create builders checkin component
    Refactor the home page to use the newly created component and display the same data as before for now.nent
    
    Refactor the home page to use the newly created component and display the same data as before for now.

---
+++ packages/nextjs/app/_components/BuildersCheckInCount.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function BuildersCheckInCount() {
+  return (
+    <p className="text-lg flex gap-2 justify-center">
+      <span className="font-bold">Checked in builders count:</span>
+      <span>To Be Implemented</span>
+    </p>
+  );
+}
+
+export default BuildersCheckInCount;
